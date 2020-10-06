900c5fcd5518eb4656173a2a479ac629b39407ba
nerjib <kabirnajib0@gmail.com>
2020-10-06 03:05:42 +0100

    update reports

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -146,5 +146,31 @@ return res.status(400).send(error);
 }
 });
 
+
+router.update('/aid', async (req, res) => {
+  const updateaid = `UPDATE reports set aid=$1 where id=$2 RETURNING *`;
+
+const values = [
+  req.body.aid,
+  moment(new Date()),
+req.body.rid
+];
+try {
+const { rows } = await db.query(updateaid, values);
+// console.log(rows);
+const data = {
+  status: 'success',
+  data: {
+    message: 'Reports Sent successfully​',
+    Name: rows[0].incidence,
+    Email: rows[0].comment,
+  },
+};
+return res.status(201).send(data);
+} catch (error) {
+return res.status(400).send(error);
+}
+});
+
 module.exports = router;
 
