40792ac92ccd95c78d3b65be1b9656c515285b9b
nerjib <kabirnajib0@gmail.com>
2020-10-06 03:18:03 +0100

    update reports

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -148,7 +148,7 @@ return res.status(400).send(error);
 
 
 router.update('/aid', async (req, res) => {
-  const updateaid = `UPDATE reports set aid=$1 where id=$2 RETURNING *`;
+  const updateaid = `UPDATE reports set aid=$1, aidtime=$2 where id=$3`;
 
 const values = [
   req.body.aid,
