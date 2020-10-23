01cb304daa0f5fbd5564e6d22630821106858b12
nerjib <kabirnajib0@gmail.com>
2020-10-23 08:11:44 +0000

    post follwup

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -320,12 +320,17 @@ const updateDraft = `INSERT INTO
  healthcentersdestroyed,    healthcentersdamaged,    healthpostsdestroyed,    healthpostsdamaged,    religiousbuildingsdestroyed,
  religiousbuildingsdamaged,    publicbuildingdestroyed,    publicbuildingdamage,    costdamageslocal,    costdamagesdolar,
 hectarescropsdamaged,    hectarescropsdestroyed,    heardsofcattle,    damagedroads,    destroyed, 
-affectedroads,    bridgesdestroyed,    bridgesdamaged,    watersourcesaffected,    wellsdestroyed
+affectedroads,    bridgesdestroyed,    bridgesdamaged,    watersourcesaffected,    wellsdestroyed,
+wellsdamaged,    otherdamages,    transport,    communication,    relief,
+tourism,    agriculture,    watersuply,    sewerage,    minery,
+energy,    industrial,    education,    commerce,    othersector,  
+health,    fisheries,    comment 
        )
   VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,
     $21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,
     $41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,
-    $61,$62,$63,$64,$65,$66,$67,$68,$69,$70,$71,$72,$73,$74,$75,$76,$77,$78,$79,$80)RETURNING *`;
+    $61,$62,$63,$64,$65,$66,$67,$68,$69,$70,$71,$72,$73,$74,$75,$76,$77,$78,$79,$80,
+    $81,$82,$83,$84,$85,$86,$87,$88,$89,$90,$91,$92,$93,$94,$95,$96,$97,$98)RETURNING *`;
   const values = [
     moment(new Date()), req.body.rid, req.body.uid, req.body.killed,   req.body.killedmen,
     req.body.killedwomen,    req.body.killedchildren,    req.body.killedelder,    req.body.missing, req.body.missingmen,
@@ -342,7 +347,11 @@ affectedroads,    bridgesdestroyed,    bridgesdamaged,    watersourcesaffected, 
   req.body.healthcentersdestroyed,   req.body.healthcentersdamaged, req.body.healthpostsdestroyed,    req.body.healthpostsdamaged,    req.body.religiousbuildingsdestroyed,  
   req.body.religiousbuildingsdamaged,req.body.publicbuildingdestroyed,    req.body.publicbuildingdamage,    req.body.costdamageslocal,    req.body.costdamagesdolar,
 req.body.hectarescropsdamaged,    req.body.hectarescropsdestroyed,    req.body.heardsofcattle,    req.body.damagedroads, req.body.destroyed,  
-req.body.affectedroads,    req.body.bridgesdestroyed,    req.body.bridgesdamaged,req.body.watersourcesaffected,    req.body.wellsdestroyed  
+req.body.affectedroads,    req.body.bridgesdestroyed,    req.body.bridgesdamaged,req.body.watersourcesaffected,    req.body.wellsdestroyed, 
+req.body.wellsdamaged,    req.body.otherdamages,req.body.transport,    req.body.communication,    req.body.relief,
+req.body.tourism, req.body.agriculture,    req.body.watersuply,    req.body.sewerage,    req.body.minery,
+req.body.energy,    req.body.industrial,    req.body.education,    req.body.commerce,req.body.othersector,  
+req.body.health,    req.body.fisheries,    req.body.comment 
  ];
 /*const values = [
 moment(new Date()), req.body.rid, req.body.uid,    req.body.killed,    req.body.killedmen,
