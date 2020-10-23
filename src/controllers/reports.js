855e6b62ff9b09d3b3ae177fd7259b1233ea8c72
nerjib <kabirnajib0@gmail.com>
2020-10-23 03:55:08 +0000

    post follwup

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -305,14 +305,11 @@ router.post('/followup', async (req, res) => {
     $81,$82,$83,$84,$85,$86,$87,$88,$89,$90,$91,$92,$93,$94,$95,$96,$97,$98) RETURNING *`;
 */
 const updateDraft = `INSERT INTO
-  followup (time, rid,    uid, killed,    killedmen,  
-    killedwomen,    killedchildren,    killedelder,    missing,    missingmen
+  followup (time, rid,    uid, killed,    killedmen  
     )
-  VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11)RETURNING *`;
+  VALUES ($1, $2,$3,$4,$5)RETURNING *`;
   const values = [
-    moment(new Date()), req.body.rid, req.body.uid, req.body.killed,   req.body.killedmen,    req.body.killedwomen, 
-    req.body.killedchildren,    req.body.killedelder,    req.body.missing, req.body.missingmen,    req.body.missingwomen
-     ];
+    moment(new Date()), req.body.rid, req.body.uid, req.body.killed,   req.body.killedmen ];
 /*const values = [
 moment(new Date()), req.body.rid, req.body.uid,    req.body.killed,    req.body.killedmen,
     req.body.killedwomen,    req.body.killedchildren,    req.body.killedelder,    req.body.missing, req.body.missingmen, 
