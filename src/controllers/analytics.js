daf46f80b5ac56100ffc6001c28d2f0f92c63814
nerjib <kabirnajib0@gmail.com>
2020-10-27 22:50:26 +0000

    upadate analytics

--- src/controllers/analytics.js
+++ src/controllers/analytics.js
@@ -6,7 +6,7 @@ const db = require('../dbs/index');
 
 
 const getFire = async () => {
-    const getfire = `select count (*) from  reports where incidence=$1`
+    const getfire = `select count (*) from  draftreports where event=$1`
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getfire,['Fire']);
@@ -21,7 +21,7 @@ const getFire = async () => {
     }
 }
 const getFLood = async () => {
-    const getflood = `select count (*) from  reports where incidence=$1`
+    const getflood = `select count (*) from  draftreports where event=$1`
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getflood,['Flood']);
@@ -36,12 +36,31 @@ const getFLood = async () => {
     }
 }
 
+const getAccident = async () => {
+  const getflood = `select count (*) from  draftreports where event=$1`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getflood,['Accident']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+}
+
 router.get('/', async (req, res) => {
   const fire = await getFire()
   const flood = await getFLood()
+  const accident = await getAccident()
+
      res.status(201).json({
          fire: fire[0].count,
-         flood: flood[0].count
+         flood: flood[0].count,
+         accident: accident[0].count
      })
 
 });
