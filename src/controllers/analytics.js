830eb1bc04a6094a80200283ac1874079337854c
nerjib <kabirnajib0@gmail.com>
2020-10-27 23:15:31 +0000

    upadate analytics

--- src/controllers/analytics.js
+++ src/controllers/analytics.js
@@ -66,5 +66,71 @@ router.get('/', async (req, res) => {
 });
 
 
+const getFireZoneA = async () => {
+  const getflood = `select count (*) from  draftreports where event=$1 and (lga=$2 or lga=$3 or lga=$4 or lga=$5
+    or lga=$6 or lga=$7 or lga=$8`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getflood,['Fire','Kaduna South','Kaduna North','Igabi', 'Chikun','Giwa','Birnin Gwari','Kajuru']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+}
+
+const getFireZoneB = async () => {
+  const getflood = `select count (*) from  draftreports where event=$1 and (lga=$2 or lga=$3 or lga=$4 or lga=$5
+    or lga=$6 or lga=$7 or lga=$8 or lga=$9`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getflood,['Fire','Zaria','Sabon Gari','Makarfi', 'Kudan','Ikara','Kubau','Soba','Lere']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+}
+const getFireZoneC = async () => {
+  const getflood = `select count (*) from  draftreports where event=$1 and (lga=$2 or lga=$3 or lga=$4 or lga=$5
+    or lga=$6 or lga=$7 or lga=$8 or lga=$9`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getflood,['Fire','Jemaa','Kauru','Kagarko', 'Kaura','Jaba','Sanga','Kachia','Zagon Kataf']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+}
+
+router.get('/zonalfire', async (req, res) => {
+  const zoneAfire = await getFireZoneA()
+  const ZoneBFire = await getFireZoneB()
+  const ZoneCFire = await getFireZoneC()
+
+  
+     res.status(201).json({
+         zoneAFire: zoneAfire[0].count,
+          ZoneBFire: ZoneBFire[0].count,
+          ZoneCFire: ZoneCFire[0].count
+       
+     })
+
+});
+
+
 module.exports = router;
 
